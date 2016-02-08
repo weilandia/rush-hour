e70f39ebc583954ad1c0656bb1e18796a13a0480
Nick Weiland <nickweiland@gmail.com>
2016-02-08 11:20:16 -0700

    Setup travis, codeclimate, waffle, coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,5 +7,23 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'capybara/dsl'
+require 'codeclimate-test-reporter'
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  Coveralls::SimpleCov::Formatter,
+  SimpleCov::Formatter::HTMLFormatter,
+  CodeClimate::TestReporter::Formatter
+])
+SimpleCov.start
+
+DatabaseCleaner.strategy = :truncation, {except: %w[public.schema_migrations]}
+
 
 Capybara.app = RushHour::Server
+
+class FeatureTest < Minitest::Test
+  include Capybara::DSL
+  include TestHelpers
+end
