6d65c670e24ab07e69ed610ae640390f28ce3982
Chelsea Johnson <cheljoh@gmail.com>
2016-02-15 15:40:18 -0700

    fixed tests

--- test/features/user_errors_test.rb
+++ test/features/user_errors_test.rb
@@ -4,18 +4,18 @@ class UserErrorsTest < FeatureTest
   include Rack::Test::Methods
 
   def test_registers_twice
-    visit '/sources/signup/jumpstartlab'
+    visit '/sources/signup'
     fill_in('identifier', with: 'jumpstartlab')
     fill_in('rootUrl', with: 'http://jumpstartlab.com')
-    click_button('Join')
+    click_button('Login')
 
-    visit '/sources/signup/jumpstartlab'
+    visit '/sources/signup'
     fill_in('identifier', with: 'jumpstartlab')
     fill_in('rootUrl', with: 'http://jumpstartlab.com')
-    click_button('Join')
+    click_button('Login')
 
     assert_equal '/sources', current_path
-    assert page.has_content?("Client jumpstartlab already exists.") #how is this working
+    assert page.has_content?("Client jumpstartlab already exists.")
   end
 
   def test_random_path
--- test/features/user_registers_test.rb
+++ test/features/user_registers_test.rb
@@ -4,17 +4,18 @@ class UserRegistersTest < FeatureTest
   include Rack::Test::Methods
 
   def test_user_registers_page_with_interface
-    visit '/sources/signup/jumpstartlab'
+    visit '/sources/signup'
+
     fill_in('identifier', with: 'jumpstartlab')
     fill_in('rootUrl', with: 'http://jumpstartlab.com')
-    click_button('Join')
+    click_button('Login')
 
     visit '/sources/jumpstartlab'
 
     within 'h1' do
       assert page.has_content?("Jumpstartlab")
     end
-    
+
     assert page.has_content?("No payload data has been received for Jumpstartlab")
   end
 
